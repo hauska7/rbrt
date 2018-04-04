9f77c62a13521a48034a31368373c58e21201296
Grzegorz Hauska <grzegorz.hauska@gmail.com>
2018-04-04 18:25:22 +0200

    attributes whitelist

--- lib/rbrt/attributes.rb
+++ lib/rbrt/attributes.rb
@@ -1,36 +1,40 @@
 class Rbrt::Attributes
-  def self.build(hash = {})
-    new(hash)
+  def self.build(whitelist: nil, content: {})
+    whitelist = content.keys if whitelist.nil?
+    new(whitelist: whitelist, content: content)
   end
 
-  def initialize(hash)
-    @hash = hash
+  def initialize(whitelist:, content:)
+    @whitelist = whitelist
+    @content = {}
+    set_content(content)
   end
 
   def method_missing(method_name, *args, &block)
-    @hash.fetch(method_name) { super }
+    @content.fetch(method_name) { super }
   end
 
+  # TODO: this is not really used in app maybe worth removing for now
   def shell?
     false
   end
 
   # TODO: fix hash interface eg for initializing active record, and more direct ==
   def to_hash
-    @hash
+    @content
   end
 
-  def set(new_attributes)
-    @hash.merge!(new_attributes.select { |attribute, _value| @hash.keys.include?(attribute) })
+  def set_content(content)
+    @content.merge!(content.select { |attribute, _value| @content.keys.include?(attribute) })
     self
   end
 
   def fetch(*args, &block)
-    @hash.fetch(*args, &block)
+    @content.fetch(*args, &block)
   end
 
   def slice(*args)
-    @hash.slice(*args)
+    @content.slice(*args)
   end
 
   private_class_method :new
