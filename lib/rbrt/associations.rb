cc5576488b2b83b9307f920452869b14e08326cf
Grzegorz Hauska <grzegorz.hauska@gmail.com>
2019-03-11 21:50:12 +0100

    association merge

--- lib/rbrt/associations.rb
+++ lib/rbrt/associations.rb
@@ -16,9 +16,9 @@ class Rbrt::Associations
     result
   end
 
-  def self.merge(associations:, other_associations:)
+  def self.merge(associations:, other_associations:, types:)
     other_associations.each do |other_association|
-      association = associations.fetch(other_association.name) { Rbrt::Association.build(type: other_association.type) }
+      association = associations.fetch(other_association.name) { Rbrt::Association.build(type: other_association.type, types:) }
       Rbrt::Association.merge(association: association, other_association: other_association)
     end
     self
@@ -32,8 +32,8 @@ class Rbrt::Associations
     self.class.clone(associations: self)
   end
 
-  def merge(associations:)
-    self.class.merge(associations: self, other_associations: associations)
+  def merge(associations:, types:)
+    self.class.merge(associations: self, other_associations: associations, types: types)
   end
 
   attr_reader :store
